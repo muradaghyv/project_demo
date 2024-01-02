bf9ad4ec059131e5436e9dcb14859b42e7164d3a
muradaghyv <139082512+muradaghyv@users.noreply.github.com>
2024-01-02 13:15:27 +0400

    Update home.py

--- home.py
+++ home.py
@@ -39,7 +39,7 @@ def main():
   if st.button("Transcribe"):
     progress_text = 'Transcibing . . .'
     my_bar = st.sidebar.progress(0, text = progress_text)
-    for percent_complete in range(300):
+    for percent_complete in range(100):
       time.sleep(0.01)
       my_bar.progress(percent_complete+1, text = progress_text)
     my_bar.empty()
